4121ac01f9241449b7bc65de9dafe9b5871e1a3b
Aditya2003tyagi <aditya2003tyagi@gmail.com>
2024-05-26 16:54:12 +0530

    added redux toolkit and created user slice

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import { errorHandler} from '../utils/error.js';
+import { errorHandler} from '..utils/error.js';
 import jwt from 'jsonwebtoken';
 
 export const signup = async (req,res, next) =>{
@@ -26,7 +26,7 @@ export const signin = async(req,res,next)=>{
         if(!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
         const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
         const {password: pass, ...rest} = validUser._doc;
-        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(r);
 
     }
     catch(error){
