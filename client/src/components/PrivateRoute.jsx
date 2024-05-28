8d34d625763976553db3d64f2403c77537216951
Aditya2003tyagi <aditya2003tyagi@gmail.com>
2024-05-28 18:08:05 +0530

    changes in private route

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -4,9 +4,4 @@ import {Navigate, Outlet, } from 'react-router-dom'
 export default function PrivateRoute() {
     const {currentUser} = useSelector((state)=> state.user)
     return currentUser ? <Outlet/> : <Navigate to='/sign-in'/>
-  return (
-    <div>
-      
-    </div>
-  )
 }
